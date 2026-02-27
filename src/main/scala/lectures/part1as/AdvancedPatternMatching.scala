88fca4d9202f001efc1645790bf91dbf7e84f772
Dorothea87 <147050615+Dorothea87@users.noreply.github.com>
2026-02-27 11:54:03 +0000

    pattern matching code along

---
+++ src/main/scala/lectures/part1as/AdvancedPatternMatching.scala
@@ -0,0 +1,52 @@
+package lectures.part1as
+
+object AdvancedPatternMatching extends App {
+  val numbers = List(1)
+  val description = numbers match {
+    case head :: Nil => println(s"The only element is $head.")
+    case _ =>
+  }
+
+  /*
+  - constants
+  - wildcards
+  - case classes
+  - tuples
+  - some special magic like above
+   */
+
+  class Person(val name: String, val age: Int)
+
+  object Person {
+    def unapply(person: Person): Option[(String, Int)] =
+      if (person.age < 21) None
+      else Some((person.name, person.age))
+
+    def unapply(age: Int): Option[String] =
+      Some(if (age < 21) "minor" else "adult")
+  }
+
+  val bob = new Person(name = "Bob", age = 25)
+  val greeting = bob match {
+    case Person(n, a) => s"Hi, my name is $n and I am $a years old."
+  }
+  println(greeting)
+
+  val legalStatus = bob.age match {
+    case Person(status) => s"My legal status is $status."
+  }
+
+  println(legalStatus)
+
+  /*
+  Exercise
+   */
+  val n: Int = 45
+  val mathProperty = n match {
+    case x if x < 10 => "single digit"
+    case x if x % 2 == 0 => "an even number"
+    case _ => "no property"
+  }
+
+
+}
